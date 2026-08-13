d749b695c7519b6c4078566f09fcea19f602d7fe
Ali Raza <aliraza315508@gmail.com>
2026-08-13 12:16:00 -0400

    added kafka retries

---
+++ payment-service/src/main/java/com/aliraza/ecommerce/paymentservice/config/KafkaRetryConfig.java
@@ -0,0 +1,18 @@
+package com.aliraza.ecommerce.paymentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaRetryConfig {
+
+    @Bean
+    public CommonErrorHandler kafkaErrorHandler() {
+        FixedBackOff fixedBackOff = new FixedBackOff(1000L, 2L);
+
+        return new DefaultErrorHandler(fixedBackOff);
+    }
+}
